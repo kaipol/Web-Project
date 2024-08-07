3b2eeaa4528638bdfcfcb3ea0ff9641d4069ea8a
kaipol <108178985+kaipol@users.noreply.github.com>
2024-08-08 02:24:39 +0800

    Delete .history directory

--- .history/.eslintrc_20240807235214.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    extends: "eslint:recommended",
-    rules: {
-        indent: ["error", "tab"],
-        "linebreak-style": ["error", "windows", "unix"],
-        quotes: ["error", "double", "single"],
-        semi: ["error", "always"],
-    },
-};
