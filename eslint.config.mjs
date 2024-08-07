c106e9a9f28fba54aa7cffeeacdbda261544708e
kaipol <1335791724@qq.com>
2024-08-08 02:23:53 +0800

    2024-8-8

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  ...tseslint.configs.recommended,
 ];
